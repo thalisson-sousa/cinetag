ff50e49c1c8a4c6edc1e5fd049b91ade9bd17cf4
thalisson-sousa <thalissonpop91@gmail.com>
2023-08-11 17:15:48 -0300

    Rodape

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,9 @@
+import styles from "./Rodape.module.css";
+
+export default function Rodape() {
+  return (
+    <footer className={styles.Rodape}>
+      <h3>Desenvolvido por Alura</h3>
+    </footer>
+  );
+}
