29256fb3cc9eda9caa1b940c3afcd2a83cff1489
thalisson-sousa <thalissonpop91@gmail.com>
2023-08-13 17:10:21 -0300

    feat: adicionando spinner de Loading

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -4,21 +4,29 @@ import Titulo from "components/Titulo";
 import { useEffect, useState } from "react";
 
 import styles from './inicio.module.css';
+import Loading from "components/Loading/Loading";
 
 export default function Inicio() {
 
  let [videos, setVideos] = useState([]);
+ let [loader, setLoader] = useState(true);
 
   useEffect(() => {
     async function getApi() {
       const data = await fetch("https://apicinetag.onrender.com");
       const res = await data.json();
       setVideos(res.Movie)
+      setLoader(false)
     }
 
     getApi();
   }, [])
-  
+
+  if(loader === true) {
+    return (
+      <Loading />
+    )
+  }
   return (
     <>
       <Banner imagem="Home" />
@@ -26,7 +34,8 @@ export default function Inicio() {
       <section className={styles.container}>
         {videos.map(video => (
           <Card key={video.id} id={video.id} titulo={video.titulo} capa={video.capa} />
-        ))}
+          ),
+        )}
       </section>
     </>
   );
